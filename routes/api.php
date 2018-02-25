e1279b440bbf303a13588ad424efa90cba526cee
Lucas Sena <lucassena.rj@gmail.com>
2018-02-25 15:40:06 -0300

    Ajust route

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('api/v1/register', 'AuthController@register');
-Route::post('api/v1/login', 'AuthController@login');
-Route::post('api/v1/recover', 'AuthController@recover');
+Route::post('v1/register', 'AuthController@register');
+Route::post('v1/login', 'AuthController@login');
+Route::post('v1/recover', 'AuthController@recover');
 
 Route::group(['middleware' => ['jwt.auth']], function() {
-    Route::get('api/v1/logout', 'AuthController@logout');
+    Route::get('v1/logout', 'AuthController@logout');
 });
